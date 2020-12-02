209d4fdece9d74d51650e304708a17431e150918
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2020-12-02 10:20:24 -0600

    clean inputs and put into a dictionary form

--- advent-of-code/dayTwo/dayTwo.py
+++ advent-of-code/dayTwo/dayTwo.py
@@ -4,12 +4,29 @@ dict = {}
 with open('inputs.txt', 'r') as file:
      inputs = [line.strip('\n') for line in file]
 
+id = 0
 for input in inputs:
-    id = 0
+    
     dataPoints = input.split(': ')
     firstPart = dataPoints[0].split(' ')
     numRange = firstPart[0].split('-')
+    numRange = [int(num) for num in numRange]
     letter = firstPart[1]
     password = dataPoints[1]
 
     print(numRange, letter, password)
+
+    tempDict = {}
+
+    tempDict["numRange"] = numRange
+    tempDict["letter"] = letter
+    tempDict["password"] = password
+
+    print(tempDict)
+
+    dict[id]=tempDict
+
+    id= id+1
+
+print(dict)
+    
