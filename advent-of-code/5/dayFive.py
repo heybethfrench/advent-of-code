d889c16a8d4262ea64e7c11460ad8372b18fb38b
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2020-12-12 17:30:04 -0600

    parsed day 5 inputs

--- advent-of-code/5/dayFive.py
+++ advent-of-code/5/dayFive.py
@@ -1,8 +1,16 @@
 #TODO Parse inputs
+
+inputs = []
+ 
+with open('inputs.txt', 'r') as file:
+     inputs = [line.strip('\n') for line in file]
+
 #TODO For each input, calculate the row number using 'F' and 'B' in the 1st 7 charachters
     # There are 127 rows
     #'F' means take the upper half
     #'B' means take the lower half
+
+
 #TODO For each input, calculate the seat number using 'R' and 'L' in the last 3 charachters
     # There are 7 columns
     #'R' means take the upper half
