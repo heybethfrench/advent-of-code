14463ce8590793b380739a7e95461456dd580f7c
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2020-12-11 20:45:57 -0600

    day 3 solution for part 2

--- advent-of-code/3/dayThree.py
+++ advent-of-code/3/dayThree.py
@@ -3,16 +3,28 @@ inputs = []
 with open('inputs.txt', 'r') as file:
      inputs = [line.strip('/n') for line in file]
   
-x = 0 %31
-index = 0
-numTrees = 0
 
-for index in range(len(inputs)):
-    position = inputs[index][x]
-    print(position)
-    if position == '#':
-        numTrees = numTrees + 1
-    x=(x+3)%31
-    index=index+1
 
-print(numTrees)
+sumTreesList = []
+
+listOfSlopes=[(1,1), (3,1), (5,1), (7,1), (1,2)]
+
+def treeCountBySlope(x_coord, y_coord):
+    x = 0 %31
+    index = 0   
+
+    numTrees = 0
+    for index in range(len(inputs)):
+        position = inputs[index][x]
+        if position == '#':
+            numTrees = numTrees + 1
+        x=(x+x_coord)%31
+        index=index+y_coord
+    return numTrees
+print(treeCountBySlope(3, 1)) #part 1
+
+print(treeCountBySlope(3, 1) *
+      treeCountBySlope(1, 1) *
+      treeCountBySlope(5, 1) *
+      treeCountBySlope(7, 1) *
+      treeCountBySlope(1, 2)) # part 2
