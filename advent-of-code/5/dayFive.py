33c92c38926dc5bbd132a701845b055d90fec3c5
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2020-12-12 17:26:15 -0600

    create files for day 5 containing inputs and todos

---
+++ advent-of-code/5/dayFive.py
@@ -0,0 +1,12 @@
+#TODO Parse inputs
+#TODO For each input, calculate the row number using 'F' and 'B' in the 1st 7 charachters
+    # There are 127 rows
+    #'F' means take the upper half
+    #'B' means take the lower half
+#TODO For each input, calculate the seat number using 'R' and 'L' in the last 3 charachters
+    # There are 7 columns
+    #'R' means take the upper half
+    #'L' means take the lower half
+#TODO Calculate unique Seat ID
+    # multiply the row number by 8, then add the column
+#TODO Part 1 goal - find the highest seat ID on a boarding pass
