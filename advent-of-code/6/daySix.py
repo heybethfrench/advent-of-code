f77d6a66e40a69700021507ed211cb9b903b217f
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2020-12-12 22:31:40 -0600

    solves daySix part 1 answer

--- advent-of-code/6/daySix.py
+++ advent-of-code/6/daySix.py
@@ -1,3 +1,30 @@
 #TODO parse input, each blank line represents another group
+
+inputs = ''
+ 
+with open('inputs.txt', 'r') as file:
+    inputs = file.read()
+   
+groups = inputs.split("\n\n")
+groups = [group.split("\n") for group in groups]
+
 #TODO calculate the number of unique questions that a group answered yes to
+
+def calcUniqueAnswers(group):
+    uniqueAnswers = []
+    for survey in group:
+        for answer in survey:
+            if answer not in uniqueAnswers:
+                uniqueAnswers.append(answer)
+    return(len(uniqueAnswers))
+
 #TODO calculate the sum of these answers
+
+def calcTotalAnswers(groups):
+    numAnswers = 0
+    for group in groups:
+        numAnswers = numAnswers + calcUniqueAnswers(group)
+    return numAnswers
+
+print(calcTotalAnswers(groups))
+        
